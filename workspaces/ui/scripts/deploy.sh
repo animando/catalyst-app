7751e7b018a350c6f5a9f568699d8db7f8e1cabe
Graham King <animandosolutions@gmail.com>
2023-01-09 18:40:33 +0000

    Overwrite config with rest endpoint id

--- workspaces/ui/scripts/deploy.sh
+++ workspaces/ui/scripts/deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-REST_API_ID=`aws apigateway get-rest-apis --query "items[?contains(name, 'dev-backend')].id" --output text`
+STAGE=dev
+REGION=eu-west-2
+REST_API_ID=`aws apigateway get-rest-apis --query "items[?contains(name, '$STAGE-backend')].id" --output text`
 
-API_ENDPOINT_BASE=https://$REST_API_ID.execute-api.eu-west-2.amazonaws.com/dev
+API_ENDPOINT_BASE=https://$REST_API_ID.execute-api.${REGION}.amazonaws.com/$STAGE
+
+echo $REST_API_ID
+echo $API_ENDPOINT_BASE
 
 export API_ENDPOINT_BASE
 
 envsubst < dist/config-env.js > dist/config.js
 rm dist/config-env.js
 
-aws s3 sync dist s3://catalyst-ui-bucket
+aws s3 sync dist s3://catalyst-ui-bucket --delete
