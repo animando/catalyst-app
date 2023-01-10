ca6c735c53aaa49d48b90a9a0c959a1c8b9fc6b4
Graham King <animandosolutions@gmail.com>
2023-01-10 09:10:27 +0000

    Add stage to endpoint base

--- workspaces/ui/scripts/create-config-js.sh
+++ workspaces/ui/scripts/create-config-js.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-STAGE=dev
 REGION=eu-west-2
 REST_API_ID=`aws apigateway get-rest-apis --query "items[?contains(name, '$STAGE-backend')].id" --output text`
 
-API_ENDPOINT_BASE=https://$REST_API_ID.execute-api.${REGION}.amazonaws.com
+API_ENDPOINT_BASE=https://$REST_API_ID.execute-api.${REGION}.amazonaws.com/$STAGE
 
 export API_ENDPOINT_BASE
 
