9b6c480a1aad2eaa9353eec55f8107413bc85609
Graham King <animandosolutions@gmail.com>
2023-01-11 13:49:44 +0000

    Fix IAM

--- workspaces/backend/services/hello/iam.ts
+++ workspaces/backend/services/hello/iam.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-template-curly-in-string */
 import { createKafkaWriteStatement, createRole } from "../../utils/iamHelpers";
-import { topic, role } from "../consumer1/config";
+import { topic } from "../consumer1/config";
+import { role } from "./config";
 
 export const iamRoleHello = createRole(role, [
   createKafkaWriteStatement(topic),
