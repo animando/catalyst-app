a51b5582ae0cb1f7c43043791ef395ce2500c7d3
Graham King <animandosolutions@gmail.com>
2023-01-11 09:22:21 +0000

    Configure msk vpc

--- workspaces/backend/services/consumer1/index.ts
+++ workspaces/backend/services/consumer1/index.ts
@@ -1,12 +1,22 @@
+/* eslint-disable no-template-curly-in-string */
 import { handlerPath } from "../../utils/handlerPath";
 import { service, topic } from "./config";
 
 export const consumer1 = {
   handler: `${handlerPath(__dirname)}/handler.handler`,
+  vpc: {
+    subnetIds: [
+      "${file(./serverlessVariables-${self:custom.stage}.yml):mskSubnetId1}",
+      "${file(./serverlessVariables-${self:custom.stage}.yml):mskSubnetId2}",
+      "${file(./serverlessVariables-${self:custom.stage}.yml):mskSubnetId3}",
+    ],
+    securityGroupIds: [
+      "${file(./serverlessVariables-${self:custom.stage}.yml):mskSecurityGroup}",
+    ],
+  },
   events: [
     {
       msk: {
-        // eslint-disable-next-line no-template-curly-in-string
         arn: "${file(./serverlessVariables-${self:custom.stage}.yml):kafkaId}",
         topic,
         consumerGroupId: service,
