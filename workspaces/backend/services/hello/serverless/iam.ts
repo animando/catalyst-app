c0da33f0f4da994068cc9e83ea2c1fb781efc5a1
Graham King <animandosolutions@gmail.com>
2023-01-13 17:37:05 +0000

    Extend lambda kafka permissions

--- workspaces/backend/services/hello/serverless/iam.ts
+++ workspaces/backend/services/hello/serverless/iam.ts
@@ -3,5 +3,6 @@ import { createRole } from "../../../utils/serverless/iamHelpers";
 import { role } from "../config";
 
 export const iamRoleHello = createRole(role, [
+  ...policies.KafkaAdmin,
   ...policies.KafkaReadWriteConsumer1,
 ]);
