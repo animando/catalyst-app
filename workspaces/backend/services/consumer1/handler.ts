0b3521413fa7815af06a17e85538ef0a8e764fad
Graham King <animandosolutions@gmail.com>
2023-01-13 17:50:27 +0000

    Debug logging of consumed event

--- workspaces/backend/services/consumer1/handler.ts
+++ workspaces/backend/services/consumer1/handler.ts
@@ -1,7 +1,11 @@
-import { MskEvent } from "../types";
-
+/* eslint-disable no-console */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const handler = async (event: MskEvent) => {
+export const handler = async (event: any) => {
   // eslint-disable-next-line no-console
   console.log("got event", event);
+
+  Object.entries(event.records).forEach((record) => {
+    console.log("got record", record);
+    console.log(JSON.stringify(record));
+  });
 };
