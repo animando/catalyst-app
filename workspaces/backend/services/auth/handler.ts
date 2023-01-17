138713095bc2c4546ebb11d1047eee5573df2c9d
Graham King <animandosolutions@gmail.com>
2023-01-17 15:22:12 +0000

    auth configuration

--- workspaces/backend/services/auth/handler.ts
+++ workspaces/backend/services/auth/handler.ts
@@ -50,7 +50,7 @@ export const handler = async (event: any) => {
     region,
     userPoolId,
     userPoolAppId,
-    userPoolDomain: `https://${userPoolDomain}.auth.${region}.amazoncognito.com`,
+    userPoolDomain: `${userPoolDomain}.auth.${region}.amazoncognito.com`,
     userPoolAppSecret,
   });
 
