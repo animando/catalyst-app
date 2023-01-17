d802fac6b1c97c2fd4aabaa5aa14e357e15a7033
Graham King <animandosolutions@gmail.com>
2023-01-17 16:12:41 +0000

    Separate pretokengeneration lambda

---
+++ workspaces/backend/serverlessPreTokenGeneration.ts
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-import-module-exports */
+import type { AWS } from "@serverless/typescript";
+import {
+  cognitoServerlessCustomConfig,
+  custom,
+  provider,
+} from "./serverlessCommonConfig";
+
+import {
+  preTokenGeneration,
+  iamRolePreTokenGeneration,
+} from "./services/preTokenGeneration/serverless";
+
+const config: AWS = {
+  service: "preTokenGeneration",
+  provider: {
+    ...provider,
+  },
+  plugins: ["serverless-esbuild"],
+  functions: {
+    preTokenGeneration,
+  },
+  resources: {
+    Resources: {
+      iamRolePreTokenGeneration,
+    },
+  },
+  custom: {
+    ...custom,
+    ...cognitoServerlessCustomConfig,
+  },
+};
+
+module.exports = config;
--- workspaces/backend/services/preTokenGeneration/handler.ts
+++ workspaces/backend/services/preTokenGeneration/handler.ts
@@ -2,5 +2,6 @@ import {} from "@aws-sdk/client-secrets-manager";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const handler = async (event: any) => {
+  // eslint-disable-next-line no-console
   console.log(event);
 };
