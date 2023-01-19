07f9cc7f5e660e30ede6891ce984e235ca9c5fd8
Graham King <animandosolutions@gmail.com>
2023-01-19 20:46:12 +0000

    Make compiled ts directory

--- workspaces/backend/scripts/tsFromJsonSchema.ts
+++ workspaces/backend/scripts/tsFromJsonSchema.ts
@@ -2,17 +2,24 @@ import { compileFromFile } from "json-schema-to-typescript";
 import fs from "fs";
 import path from "path";
 
+const outDir = path.resolve(__dirname, "../compiled-types");
+const schemaDir = path.resolve(__dirname, "../json-schemas");
+
 const compileSchema = async (schema: string) => {
-  const schemaFile = path.resolve(__dirname, `../json-schemas/${schema}.json`);
+  const schemaFile = path.resolve(schemaDir, `${schema}.json`);
   // compile from file
   const ts = await compileFromFile(schemaFile);
-  fs.writeFileSync(
-    path.resolve(__dirname, "../compiled-types", `${schema}.d.ts`),
-    ts
-  );
+  fs.writeFileSync(path.resolve(outDir, `${schema}.d.ts`), ts);
+};
+
+const mkdir = () => {
+  if (!fs.existsSync(outDir)) {
+    fs.mkdirSync(outDir);
+  }
 };
 
 const run = async () => {
+  mkdir();
   const schemas = ["Consumer1Message", "SnsTopic1Payload"];
 
   await Promise.all(schemas.map(compileSchema));
